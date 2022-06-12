d416a2df9bee3190110642f4a587fa8a1181e7d3
jmattgiroux <jmattgiroux@gmail.com>
2022-06-12 02:13:05 -0600

    while working on team assignment I noticed differences in my code for the personal assignment

--- teamL02/backend/database/connect.js
+++ teamL02/backend/database/connect.js
@@ -1,10 +1,34 @@
 /**
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ * File uses insights and code from
+ * https://github.com/byui-cse/cse341-code-student/blob/L02-team-solution-stretch/backend/db/connect.js
  */
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb").MongoClient;
 const dotenv = require("dotenv");
 dotenv.config();
+
+let database;
+
+const initDatabase = (callback) => {
+  // if it already exists, you don't need to re-initialize the database.
+  if (database) {
+    console.log("Database is already initialized!!!");
+    return callback(null, database);
+  }
+
+  // Examples: https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html
+  MongoClient.connect(uri)
+    .then((client) => {
+      database = client;
+      callback(null, database);
+    })
+    .catch((error) => {
+      if (!database) {
+        callback(error);
+      }
+    });
+};
 
 async function main() {
   const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rzqr5td.mongodb.net/?retryWrites=true&w=majority`;
