27eac588043c976b0fdb80f815d5ed2039ba5fad
jmattgiroux <jmattgiroux@gmail.com>
2022-06-11 23:26:50 -0600

    added instructor's solutions as comments; I know understand better that we're still supposed to use MVC for this class; having the database actually in the computer threw me for a loop, and I didn't initially understand that we were supposed to set up our own database rather than make a request to an existing one.

--- teamL02/backend/routes/index.js
+++ teamL02/backend/routes/index.js
@@ -8,3 +8,16 @@ routes.get("/", (req, res) => {
 });
 
 module.exports = routes;
+
+// instructor solution below:
+
+// const express = require('express');
+
+// const professionalController = require('../controllers/professional');
+
+// const router = express.Router();
+
+// // GET /feed/posts
+// router.get('/', professionalController.getData);
+// // localhost:8080/professional/
+// module.exports = router;
