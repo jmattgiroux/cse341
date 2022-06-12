ce474a34a9f9815a183997c04784e7c02076d4dd
jmattgiroux <jmattgiroux@gmail.com>
2022-06-12 02:18:17 -0600

    edited professional.js

---
+++ teamL02/backend/controllers/professional.js
@@ -0,0 +1,17 @@
+/**
+ * File uses code and insights from
+ * https://github.com/byui-cse/cse341-code-student/blob/L02-team-solution-stretch/backend/controllers/professional.js
+ */
+
+// If I can get this to work, then I'll rename mongodb to mongoDatabase.
+const mongodb = require('../database/connect');
+
+const getData = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('user').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]); // we just need the first one (the only one)
+  });
+};
+
+module.exports = { getData };
