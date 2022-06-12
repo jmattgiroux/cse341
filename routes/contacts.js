ce47b667fbf149b2dee80ba0de88556cc1e9f5fe
jmattgiroux <jmattgiroux@gmail.com>
2022-06-12 00:56:53 -0600

    Moved toArray part of instructor code in contacts-controller over to here; will need to add the rest of the code from contacts-controller over to whatever makes the requests to the routers here.

---
+++ routes/contacts.js
@@ -0,0 +1,17 @@
+// routes are just code for handling urls
+// see here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+// This file uses code and insights from:
+// https://github.com/byui-cse/cse341-code-student/blob/L02-personal-solution/routes/contacts.js
+
+const router = require("express").Router();
+
+const contactsController = require("../controllers/get-contacts");
+
+// router.get() documentation: https://expressjs.com/en/guide/routing.html
+
+// Moved toArray part of instructor code in contacts-controller over to here; will need to add the rest of the code from contacts-controller over to whatever makes the requests to the routers here.
+router.get("/", contactsController.getAllContacts).toArray();
+
+router.get("/:id", contactsController.getSingleContact).toArray();
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 // routes are just code for handling urls
 // see here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+// This file uses code and insights from:
+// https://github.com/byui-cse/cse341-code-student/blob/L02-personal-solution/routes/index.js
 
 const router = require('express').Router();
 
-router.get("/", (req, res) => {
-    res.send("Hi Jay Giroux!");
-
-});
+// .use() documentation: https://www.geeksforgeeks.org/express-js-router-use-function/
+// router.use( path, function )
+router.use("/contacts", require("./contacts"));
 
 module.exports = router;
