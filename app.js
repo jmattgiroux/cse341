3067cf037b8a298365a5b86ade864ef6f922ae12
jmattgiroux <jmattgiroux@gmail.com>
2022-06-12 01:23:08 -0600

    Updated app.js to use instructor's solution code; line 14 may turn out to be an issue due to how we've made changes to get-contacts.js to get rid of the extra parameters.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // Heroku Test Deployment
 // Resource used: https://www.freecodecamp.org/news/how-to-deploy-an-application-to-heroku/
+// This file uses code and insights from:
+// https://github.com/byui-cse/cse341-code-student/blob/L02-personal-solution/app.js
+
+const bodyParser = require("body-parser");
+const mongoDatabase = require("./database/connect");
 
 const app = require("express")();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.use("/", require("./routes"));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-
+mongoDatabase.initDatabase((error, mongoDatabase) => {
+  if (error) {
+    console.log(error);
+  } else {
+    app.listen(PORT);
+    console.log(`Connected to database and listening on ${PORT}`);
+  }
 });
