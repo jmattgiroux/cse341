46a14605ff13c22a7a3ca19413593ecfd46eaf7b
jmattgiroux <jmattgiroux@gmail.com>
2022-06-18 17:39:31 -0600

    Hopefully this will fix the issues with swagger

--- personal04/routes/index.js
+++ personal04/routes/index.js
@@ -4,14 +4,7 @@
 // https://github.com/byui-cse/cse341-code-student/blob/L02-personal-solution/routes/index.js
 
 const router = require("express").Router();
-// https://www.npmjs.com/package/swagger-ui-express
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
 
-// .use() documentation: https://www.geeksforgeeks.org/express-js-router-use-function/
-// router.use( path, function )
-
-// from what I understand, if there's a request to the server for /contacts, then the stuff from contacts.js file will handle the request. For example, if the request is just .../contacts, then the getAllContacts function will be called in contacts.js. It seems we're making an API for our server.
 router.use("/contacts", require("./contacts"));
 router.use("/", require("./swagger"));
 
