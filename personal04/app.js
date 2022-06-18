c3c78a26e12e2cc75f5712a295d188d1221eff15
jmattgiroux <jmattgiroux@gmail.com>
2022-06-18 15:22:07 -0600

    Added stuff from assignment video to get it to work with react app

--- personal04/app.js
+++ personal04/app.js
@@ -2,6 +2,7 @@
 
 // most of code below was imported from Team L04 Assignment from CSE 341 Course
 // https://www.npmjs.com/package/cors
+const bodyParser = require("body-parser");
 const cors = require("cors");
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -12,6 +13,27 @@ const mongoDatabase = require("./database/connect");
 const PORT = process.env.PORT || 8080;
 
 app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://cse341-contacts-frontend.netlify.app/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader('Content-Type', 'application.json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  // .use(cors())
+  // .use(express.json())
+  // .use(express.urlencoded({ extended: true }))
+  .use("/", require("./routes"));
+
+  app
+  .use(bodyParser.json())
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
