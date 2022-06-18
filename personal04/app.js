56e1d426f03725ece5af09ba03e0b3b42569c7f4
jmattgiroux <jmattgiroux@gmail.com>
2022-06-18 17:26:57 -0600

    Added extra stuff to app.js to make it work with the react app

--- personal04/app.js
+++ personal04/app.js
@@ -14,15 +14,18 @@ const PORT = process.env.PORT || 8080;
 
 app
   .use(bodyParser.json())
-  // .use(cors())
-  // .use(express.json())
-  // .use(express.urlencoded({ extended: true }))
   .use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
       "*"
       // "https://cse341-contacts-frontend.netlify.app/"
     );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
   })
   .use("/", require("./routes"));
