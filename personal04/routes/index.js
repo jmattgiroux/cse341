308c0cc6d52738997aa0d365fbdeeffaff0f4d96
jmattgiroux <jmattgiroux@gmail.com>
2022-06-18 18:28:40 -0600

    More swagger experiments

--- personal04/routes/index.js
+++ personal04/routes/index.js
@@ -5,7 +5,7 @@
 
 const router = require("express").Router();
 
-router.use("/contacts", require("./contacts"));
 router.use("/", require("./swagger"));
+router.use("/contacts", require("./contacts"));
 
 module.exports = router;
